461dce4ef769a3f069301b5ab8bc859881214ecf
bruno.paiva2009@gmail.com <bruno.paiva2009@gmail.com@776ecbdd-94de-3ffc-a97b-c78998030f84>
2012-04-26 11:12:57 +0000

    Atualização IDAOPermissao

--- trunk/rascontrolweb/IDAO/IDAOPermissao.cs
+++ trunk/rascontrolweb/IDAO/IDAOPermissao.cs
@@ -8,15 +8,11 @@ namespace IDAO
 {
     public interface IDAOPermissao
     {
-        List<Permissao> SelectAllPermissao();
-        List<Permissao> SelectAllPermissaoUsuario(int id);
-        List<Permissao> ConsultarPermissao(Permissao permissao);
-        
-        int VerificarPermissaoExistePorDescricao(string descricao);
-        void InsertPermissao(Permissao permissao);
+        List<Permissao> ConsultarAllPermissao();
+        Permissao ConsultarPermissaoCodigo(int codigo);
+        List<Permissao> ConsultarAllPermissaoFiltros(int codigo, string descricao);
+        void CadastrarPermissao(Permissao permissao);
         void UpdatePermissao(Permissao permissao);
-        Permissao SelectPermissaoPorCodigo(int id);
-        void DeletePermissaoPorCodigo(int id);
-        Permissao SelectPermissaoPorNome(string nome);
+        void DeletePermissao(int id);
     }
 }
