6c91ab32e82247f5bd3ab2379b1f502dc2e3c4f0
bruno.paiva2009@gmail.com <bruno.paiva2009@gmail.com@776ecbdd-94de-3ffc-a97b-c78998030f84>
2012-05-02 23:49:53 +0000

    All Classes - FINAL

---
+++ trunk/RasControlTotal/RasControl/IDAO/IDAOEstoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassesBasicas;
+
+namespace IDAO
+{
+  public interface IDAOEstoria
+  {
+    List<Estoria> ConsultarAllEstoria();
+    Estoria ConsultarEstoriaCodigo(int codigo);
+    List<Estoria> ConsultarAllEstoriaFiltros(int codigo, string descricao);
+    void CadastrarEstoria(Estoria estoria);
+    void UpdateEstoria(Estoria estoria);
+    void DeleteEstoria(int id); 
+  }
+}
